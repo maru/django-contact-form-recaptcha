451bc346b81ffa20e724dbe41b92f2d4bd8c674d
maru <github@s106.org>
2018-04-09 11:21:53 +0200

    Update to django-contact-form v1.5

--- setup.py
+++ setup.py
@@ -2,24 +2,23 @@ import os
 
 from setuptools import setup
 
-setup(name='django-contact-form',
-      version='1.3.1',
+
+setup(name='django-contact-form-recaptcha',
+      version='1.6.0',
       zip_safe=False, # eggs are the devil.
-      description='Generic contact-form application for Django',
+      include_package_data=True,
+      description='A generic contact-form application for Django',
       long_description=open(os.path.join(os.path.dirname(__file__),
                                          'README.rst')).read(),
-      author='Maru Berezin',
-      url='https://github.com/maru/django-contact-form/',
+      author='James Bennett, Maru Berezin',
+      url='https://github.com/maru/django-contact-form-recaptcha/',
       packages=['contact_form', 'contact_form.tests'],
-      package_data={'contact_form': [ 'locale/*/*/*',
-            'templates/contact_form/*.html', 'templates/contact_form/*.txt']},
       test_suite='contact_form.runtests.run_tests',
-      install_requires=['django-recaptcha==1.0.5'],
       classifiers=['Development Status :: 5 - Production/Stable',
                    'Environment :: Web Environment',
                    'Framework :: Django',
-                   'Framework :: Django :: 1.8',
-                   'Framework :: Django :: 1.9',
+                   'Framework :: Django :: 1.11',
+                   'Framework :: Django :: 2.0',
                    'Intended Audience :: Developers',
                    'License :: OSI Approved :: BSD License',
                    'Operating System :: OS Independent',
@@ -27,8 +26,15 @@ setup(name='django-contact-form',
                    'Programming Language :: Python :: 2',
                    'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.3',
                    'Programming Language :: Python :: 3.4',
                    'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
                    'Topic :: Utilities'],
-      )
+      install_requires=[
+          'Django>=1.11',
+      ],
+      extras_require={
+          'akismet': ['akismet'],
+          'captcha': ['django-recaptcha'],
+      },
+)
