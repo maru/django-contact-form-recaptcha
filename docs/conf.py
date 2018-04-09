f43a2b925b717113f4da42191888cb8cc943e685
maru <github@s106.org>
2018-04-09 11:42:49 +0200

    Update to django-contact-form v1.5

--- docs/conf.py
+++ docs/conf.py
@@ -6,10 +6,10 @@ extensions = []
 templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
-project = u'django-contact-form'
-copyright = u'2007-2015, James Bennett'
-version = '1.3'
-release = '1.3'
+project = u'django-contact-form-recaptcha'
+copyright = u'2007-2018, James Bennett, Maru Berezin'
+version = '1.6.0'
+release = '1.6.0'
 exclude_trees = ['_build']
 pygments_style = 'sphinx'
 html_static_path = ['_static']
@@ -22,4 +22,3 @@ if not on_rtd:
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
